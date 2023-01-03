43040fd1d030078e0dca0ce713bedf2a14b7bdbb
liuyulong <2057890701@qq.com>
2023-01-03 14:12:29 +0800

    登录页面完成

--- src/api/user.js
+++ src/api/user.js
@@ -1,6 +1,13 @@
-// import request from '@/utils/request'
+import request from '@/utils/request'
 
-export function login(data) {}
+export function login(data) {
+	// 返回一个axios对象 => promise  // 返回了一个promise对象
+	return request({
+		url: '/sys/login',
+		method: 'post',
+		data
+	})
+}
 
 export function getInfo(token) {}
 
